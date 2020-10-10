4e10552df958d9e048a4fe29a4aabde1d69b60fc
Qynklee <anhtu16101999@gmail.com>
2020-10-10 22:18:19 +0700

    Thêm Search Page/ Chưa chỉnh button search chạy

---
+++ TuHuTuHu/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TuHuTuHu.Models;
+
+namespace TuHuTuHu.Controllers
+{
+    public class SearchController : Controller
+    {
+        MyDBContext dbContext = new MyDBContext();
+        Account acc;
+        List<Account> resultAccounts;
+
+
+        // GET: Search
+        public ActionResult Index(string searchString)
+        {
+            acc = dbContext.Account.Find(Convert.ToInt32(Session["userID"]));
+
+            
+            if (searchString == null)
+            {
+                return null;
+            }
+            else
+            {
+                resultAccounts = dbContext.Account.Where(x => x.Username.Contains(searchString)).ToList();
+
+                ResultSearch result = new ResultSearch();
+                result.account = acc;
+                result.resultAccounts = resultAccounts;
+                return View(result);
+            }
+        }
+    }
+}
