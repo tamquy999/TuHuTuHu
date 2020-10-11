26cb7d7ca563f4d692cd012ba49aad301e6f02d8
Qynklee <anhtu16101999@gmail.com>
2020-10-12 00:26:15 +0700

    Fix lại user post / xóa model UserPost

--- TuHuTuHu/Controllers/PostController.cs
+++ TuHuTuHu/Controllers/PostController.cs
@@ -11,34 +11,16 @@ namespace TuHuTuHu.Controllers
     {
         MyDBContext db = new MyDBContext();
         Post thisPost = new Post();
-        Account ownerPost = new Account();
         Account myAcc = new Account();
-        List<Comment> allComments;
 
-        UserPost userPost = new UserPost();
-        // GET: Post
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="userID">userID của người chủ post</param>
-        /// <param name="postID">postID của bài viết cần xem</param>
-        /// <returns></returns>
-        public ActionResult Index(string userID, string postID)
+        public ActionResult Index(string postID)
         {
             myAcc = db.Account.Find(Convert.ToInt32(Session["userID"]));
             
-            if (postID != "" && postID != null && userID != "" && userID != null)
+            if (postID != "" && postID != null && myAcc != null)
             {
-                ownerPost = db.Account.Find(Convert.ToInt32(userID));
                 thisPost = db.Post.Find(Convert.ToInt32(postID));
-                allComments = userPost.GetAllCommentPerPost(db, postID);
-
-                userPost.myAcc = this.myAcc;
-                userPost.ownerPost = this.ownerPost;
-                userPost.thisPost = this.thisPost;
-                userPost.allComments = this.allComments;
-
-                return View(userPost);
+                return View(thisPost);
             }
             else
             {
