fff6113e5a7430064e7b62e6a4950ee66969c2a4
tamquy999 <tamquy999@gmail.com>
2020-10-14 14:25:59 +0700

    Add userpage

---
+++ TuHuTuHu/Controllers/UserpageController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TuHuTuHu.Models;
+
+namespace TuHuTuHu.Controllers
+{
+    public class UserpageController : Controller
+    {
+        MyDBContext dbContext = new MyDBContext();
+        Account acc = new Account();
+
+        // GET: Userpage
+        public ActionResult Index(int id)
+        {
+            acc = dbContext.Account.Find(Convert.ToInt32(Session["userID"]));
+
+            ViewBag.CurrentUser = acc;
+            ViewBag.Contacts = GetAllContact();
+
+            Userpage userpage = new Userpage();
+
+            userpage.account = dbContext.Account.Find(id);
+            userpage.posts = dbContext.Post.Where(s => s.UserID == id).ToList();
+
+            return View(userpage);
+        }
+
+        List<Account> GetAllContact()
+        {
+            List<Msg> messages = dbContext.Msg.Where(s => s.Account.AccID == acc.AccID || s.Account1.AccID == acc.AccID).ToList();
+            List<string> contactIDs = new List<string>();
+            foreach (var message in messages)
+            {
+                // Neu minh la nguoi gui thi lay id nguoi nhan va nguoc lai
+                if (message.Account.AccID == acc.AccID)
+                {
+                    contactIDs.Add(message.Account1.AccID.ToString());
+                }
+                else contactIDs.Add(message.Account.AccID.ToString());
+            }
+            List<Account> contacts = dbContext.Account.Where(s => contactIDs.Contains(s.AccID.ToString())).ToList();
+            return contacts;
+        }
+
+    }
+}
---
+++ TuHuTuHu/Models/Userpage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TuHuTuHu.Models
+{
+    public class Userpage
+    {
+        public Account account { get; set; }
+
+        public List<Post> posts { get; set; }
+    }
+}
