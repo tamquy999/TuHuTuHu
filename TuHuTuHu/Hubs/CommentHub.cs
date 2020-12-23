76c742c8c260314760a24d12afc054fddb34f7ec
tamquy999 <tamquy999@gmail.com>
2020-12-23 23:48:29 +0700

    Comment OK, sửa nút gửi tn

--- TuHuTuHu/Hubs/CommentHub.cs
+++ TuHuTuHu/Hubs/CommentHub.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using TuHuTuHu.Models;
 
 namespace TuHuTuHu.Hubs
 {
     public class CommentHub : Hub
     {
-        
+        public void Comment(int postID, int userID, string content)
+        {
+            MyDBContext db = new MyDBContext();
+
+            Comment cmt = new Comment();
+            cmt.PostID = postID;
+            cmt.UserID = userID;
+            cmt.Content = content;
+
+            db.Comment.Add(cmt);
+
+            //db.SaveChanges();
+
+            Account acc = db.Account.Where(s => s.AccID == userID).FirstOrDefault();
+
+            Clients.All.addCommentToPost(postID, acc.AccID, acc.AvtLink, acc.Fullname, content);
+        }
     }
 }
