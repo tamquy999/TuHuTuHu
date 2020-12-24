c3d937e43f740de74c63d500a46f29a3e72613e9
Qynklee <anhtu16101999@gmail.com>
2020-12-24 23:35:55 +0700

    fix Hashpass override

--- TuHuTuHu/Controllers/LoginController.cs
+++ TuHuTuHu/Controllers/LoginController.cs
@@ -17,25 +17,6 @@ namespace TuHuTuHu.Controllers
             return View();
         }
 
-        private string HashPass(string pass)
-        {
-            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
-            {
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(pass);
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
-
-                // Convert the byte array to hexadecimal string
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < hashBytes.Length; i++)
-                {
-                    sb.Append(hashBytes[i].ToString("X2"));
-                }
-                //return sb.ToString();
-                return pass;
-            }
-        }
-
-
         [HttpPost]
         public ActionResult Index(string user, string pass)
         {
